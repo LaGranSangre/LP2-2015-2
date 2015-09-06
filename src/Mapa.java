2a91942ea9213401995305397edb3d8213e18cbd
Sebastian-Dioses <sebastian.dioses@gmail.com>
2015-09-06 11:51:55 -0500

    Dejen de modificar mapa

--- src/Mapa.java
+++ src/Mapa.java
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
-
 public class Mapa {
 
 }
-=======
-
->>>>>>> origin/master
-public class Mapa {
-
-	public class Mapa {
-	
-	}
-
-}
-//efsssafadsfalalalala
-//POL
-<<<<<<< HEAD
-//causa ya fue
-=======
->>>>>>> origin/master
->>>>>>> origin/master
