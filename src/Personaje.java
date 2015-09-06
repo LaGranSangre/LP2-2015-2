fcf1bb09016271ec758c13faee521ec289c7bb69
vfuentesr <vfuentesr@pucp.pe>
2015-09-06 12:46:38 -0500

    perso

--- src/Personaje.java
+++ src/Personaje.java
@@ -1,4 +1,19 @@
 
 public class Personaje {
-
+	private int x;
+	private int y;
+	
+	private int getX() {
+		return x;
+	}
+	private void setX(int x) {
+		this.x = x;
+	}
+	private int getY() {
+		return y;
+	}
+	private void setY(int y) {
+		this.y = y;
+	}
 }
+
