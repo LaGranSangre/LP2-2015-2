e08d08b5f6a651bef6b9c8c60ab7ceb5327b8d4d
LuisLoo <loo.luis@pucp.pe>
2015-09-06 15:46:06 -0500

    celda full falta mapa =(

--- src/Celda.java
+++ src/Celda.java
@@ -2,11 +2,11 @@ public class Celda {
 	private char cel;
 	private int tipo;
 
-	private int getTipo() {
+	public int getTipo() {
 		return tipo;
 	}
 
-	private void setTipo(int tipo) {
+	public void setTipo(int tipo) {
 		switch(tipo){
 		case 'D':  //Terreno duo
 			tipo = 1;
@@ -22,6 +22,21 @@ public class Celda {
 			break;
 		case ' ':  //Terreno vacio
 			tipo = 5;
+			break;
+		case 'g': //Obstaculo A: Rocas gigantes
+			tipo = 6;
+			break;
+		case 'n': //Obstaculo B:  Un barranco
+			tipo = 7;
+			break;
+		case 'p': //Paso alto(p) - Terreno Impasable
+			tipo = 8;
+			break;
+		case 'h': //Es un surco o hueco.
+			tipo = 9;
+			break;
+		case 'H': //Hueco o surco grande.(H)
+			tipo = 10;
 			break;
 		}
 	}
--- src/Mapa.java
+++ src/Mapa.java
@@ -1,3 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
 public class Mapa {
+	private int numFil;
+	private int numCol;
+	//private List<List<Celda>> mapa;
+	private List<List<String>> lista;
 	
+	public int getNumCol() {
+		return numCol;
+	}
+	
+	public void setNumCol(int numCol) {
+		this.numCol = numCol;
+	}
+	
+	public int getNumFil() {
+		return numFil;
+	}
+	
+	public void setNumFil(int numFil) {
+		this.numFil = numFil;
+	}
+	
+	public Mapa(){ //Constructor de mapa
+		//Las dimensiones del mapa son 12*16(12 filas y 16 columnas)
+		numFil = 12; 
+		numCol = 16;
+		
+		/*for (List<Celda> list : mapa) {
+			for (Celda celda : list) {
+				celda.
+			}
+		}*/
+	}
 }
