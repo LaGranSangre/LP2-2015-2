74f464d566233a4d979fbd255fc7f454d82ac5c2
vfuentesr <vfuentesr@pucp.pe>
2015-09-06 13:58:48 -0500

    personaje

--- src/Personaje.java
+++ src/Personaje.java
@@ -1,19 +1,49 @@
 
 public class Personaje {
-	private int x;
-	private int y;
+	private int posX;
+	private int posY;
 	
-	private int getX() {
-		return x;
+	public Personaje(int posX, int posY){
+		this.setPosX(posX);
+		this.setPosY(posY);
 	}
-	private void setX(int x) {
-		this.x = x;
+
+	public int getPosX() {
+		return posX;
 	}
-	private int getY() {
-		return y;
+
+	public void setPosX(int posX) {
+		this.posX = posX;
 	}
-	private void setY(int y) {
-		this.y = y;
+
+	public int getPosY() {
+		return posY;
 	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+	
+}
+
+class PersonajePrincipal extends Personaje{
+	private static int vida = 10;
+	
+	public PersonajePrincipal(){
+		
+	}
+	
+	private static int getVida() {
+		return vida;
+	}
+
+	private static void setVida(int vida) {
+		PersonajePrincipal.vida = vida;
+	}
+	
+}
+
+class Enemigo extends Personaje{
+	
 }
 
