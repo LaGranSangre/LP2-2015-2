12f8f0c44c769177e5305e2b206ea59181a3c57a
Sebastian-Dioses <sebastian.dioses@gmail.com>
2015-09-06 12:48:33 -0500

    juego

--- src/juego.java
+++ src/juego.java
@@ -1,8 +1,22 @@
 
 public class juego {
+	private Personaje cristobal;
+	private GestorMapas gestor;
+	private Mapa mapa_actual;
+	
+	
+	public juego(){
+		cristobal=new Personaje();
+		mapa_actual=new Mapa();
+		gestor=new GestorMapas();
+		//aqui se añade el metodo de gestor que llena el mapa con informacion del archivo
+		//de texto		
+	}
+	
 	public static void main(String args[]) {
-		// MiVentana ventana = new MiVentana();
-		 } 
+		juego game=new juego();
+		
+		} 
 
 }
 
