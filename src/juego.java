6000b29bd4ca468f50385cc7b5343a73b7b5b811
marcelomilera <a20111371@pucp.pe>
2015-09-08 19:00:53 -0500

    puede volver a jugar despues de haber perdido

--- src/juego.java
+++ src/juego.java
@@ -30,7 +30,7 @@ public class juego {
 	public int MatarEnemigo(String str){
 		str.toUpperCase();
 			
-		if(str.compareTo("WDEWW")==0){
+		if(str.compareTo("WDEWW")==0 || str.compareTo("wdeww")==0){
 			mapa_actual.setEnemigo(false);
 			gestor.remover_enemigo((mapa_actual));
 			return 1;
@@ -256,24 +256,114 @@ public class juego {
 		Scanner s = new Scanner(System.in);
 		String str = s.nextLine();		
 		String clave="";
+		String clave_min="";
 		int nivel = mapa_actual.getNivel();
 		
 		switch (nivel){
 			case 0:
 				clave = "WSIKDDLL";
+				clave_min="wsikddll";
 				break;
 			case 1:
-				if(cristobal.getPosX()==5 && hermana.getPosX()==5)
+				if(cristobal.getPosX()==5 && hermana.getPosX()==5){
 					clave = "IWOELD";
-				else 
+					clave_min="iwoeld";
+				}
+				else {
+					clave_min="wuoqei";
 					clave = "WUOQEI";
+				}
 				break;
 			case 2:
+				clave_min="ldoequoe";
 				clave ="LDOEQUOE";				
 				break;
 		}
+		return (str.equals(clave)||str.equals(clave_min));
+	}
 
-		return (str.equals(clave));
+	
+	public void Jugar(){
+		while(true){
+			EmpezarJuego();
+		}
+	}
+	
+	public void EmpezarJuego(){
+		int opcion;
+		if(this.PantallaInicio()==0) //Salir del juego
+			return;
+
+		
+		Scanner s = new Scanner(System.in);		
+		String str;
+		while(true){
+			if(cristobal.getVida()<=0){
+				System.out.println("GAME OVER!! FIN DEL JUEGO");
+				this.FinalizarJuego();
+				return;
+			}
+			int posCX = cristobal.getPosX(), posCY = cristobal.getPosY(), posHX = hermana.getPosX(), posHY = hermana.getPosY();
+			
+			this.Rend.mostrarMapa(this.mapa_actual,cristobal,hermana);
+			
+			int tipoCeldaH = this.mapa_actual.getMapa()[posHY][posHX].getTipo(), tipoCeldaC = this.mapa_actual.getMapa()[posCY][posCX].getTipo();  
+			
+			if(tipoCeldaC==1 && tipoCeldaH==1){ //tipo 1 : 'D'
+				while(true){
+					this.Rend.mostrarAccionDuo(this.mapa_actual.getNivel(), cristobal.getPosX(), hermana.getPosX());
+					if(this.VerificarAccionDuo()){
+						this.AccionDuo(this.mapa_actual);
+						break;
+					}
+					else{
+						cristobal.setVida(cristobal.getVida()-2);
+						if(cristobal.getVida()<=0){
+							System.out.println("GAME OVER!! FIN DEL JUEGO");
+							this.FinalizarJuego();
+							return;
+						}
+					}
+				}
+			}
+
+					
+							 
+			if(this.mapa_actual.isEnemigo()==true && tipoCeldaC==2){
+				while(true){
+					this.Rend.mostrarAccionCristobal();
+					str=s.nextLine();
+					if(this.MatarEnemigo(str)==1)break;
+				}				
+			}
+			str= s.nextLine();
+			if(str.length()==1 ){
+				this.enviar_comando(str.charAt(0));
+
+			
+			//Si llega al final del nivel
+			if((posCX==(this.mapa_actual.getNumCol()-1)) && (posHX==posCX)){							
+				this.mapa_actual.setNivel(this.mapa_actual.getNivel()+1);
+				this.Rend.mostrarHistoria(this.mapa_actual.getNivel());
+				this.gestor.CargarMapa(this.mapa_actual, cristobal,hermana);//carga el mapa siguiente
+				this.Rend.mostrarMapa(this.mapa_actual, cristobal, hermana);
+			}		
+
+			}
+		}
+		
+	}
+	
+	public void FinalizarJuego(){
+		//reinicializar variables
+		PersonajePrincipal.setVida(10);
+		cristobal=new PersonajePrincipal();
+		hermana=new PersonajePrincipal();
+		gestor=new GestorMapas();
+		mapa_actual=new Mapa();
+		gestor.CargarMapa(mapa_actual, cristobal,hermana);
+		Interprete=new InterpreteComandos();
+		Rend=new Renderizador();
 	}
 	
 	public static void main(String args[]) {
@@ -286,56 +376,7 @@ public class juego {
 		m.setNivel(2);
 		gm.CargarMapa(m, cristobal);
 		m.MostrarMapa();*/
+		game.Jugar();
 		
-		int opcion;
-		if(game.PantallaInicio()==0) //Salir del juego
-			return;
-
-		
-		Scanner s = new Scanner(System.in);		
-		String str;
-		while(true){
-			int posCX = cristobal.getPosX(), posCY = cristobal.getPosY(), posHX = hermana.getPosX(), posHY = hermana.getPosY();
-			
-			game.Rend.mostrarMapa(game.mapa_actual,cristobal,hermana);
-			
-			int tipoCeldaH = game.mapa_actual.getMapa()[posHY][posHX].getTipo(), tipoCeldaC = game.mapa_actual.getMapa()[posCY][posCX].getTipo();  
-			
-			if(tipoCeldaC==1 && tipoCeldaH==1){ //tipo 1 : 'D'
-				while(true){
-					game.Rend.mostrarAccionDuo(game.mapa_actual.getNivel(), cristobal.getPosX(), hermana.getPosX());
-					if(game.VerificarAccionDuo()){
-						game.AccionDuo(game.mapa_actual);
-						break;
-					}
-					else
-						cristobal.setVida(cristobal.getVida()-2);
-				}
-			}
-
-					
-							 
-			if(game.mapa_actual.isEnemigo()==true && tipoCeldaC==2){
-				while(true){
-					game.Rend.mostrarAccionCristobal();
-					str=s.nextLine();
-					if(game.MatarEnemigo(str)==1)break;
-				}				
-			}
-			str= s.nextLine();
-			if(str.length()==1 ){
-				game.enviar_comando(str.charAt(0));
-
-			
-			//Si llega al final del nivel
-			if((posCX==(game.mapa_actual.getNumCol()-1)) && (posHX==posCX)){							
-				game.mapa_actual.setNivel(game.mapa_actual.getNivel()+1);
-				game.Rend.mostrarHistoria(game.mapa_actual.getNivel());
-				game.gestor.CargarMapa(game.mapa_actual, cristobal,hermana);//carga el mapa siguiente
-				game.Rend.mostrarMapa(game.mapa_actual, cristobal, hermana);
-			}		
-
-			}
-		}
 	}
 }
